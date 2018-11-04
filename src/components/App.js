bf9a10b7c5bc51e47780b8bd45f29d5962167863
StudentOfJS <rodlewis45@gmail.com>
2018-11-04 13:24:25 +0800

    recipe detail reducer and display

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,113 @@
-import React, { Fragment, useReducer, useEffect, memo } from "react";
+import React, {
+  Fragment,
+  useReducer,
+  useEffect,
+  memo,
+  lazy,
+  Suspense
+} from "react";
 import { fetch } from "whatwg-fetch";
 import Header from "./Header";
-import RecipeList from "./Recipe/RecipeList";
 import RecipeDetail from "./Recipe/RecipeDetail";
+const RecipeList = lazy(() => import("./Recipe/RecipeList"));
 
 const recipesReducer = (state, action) => {
-  console.log(JSON.stringify(state));
-  if (JSON.stringify(state) !== JSON.stringify(action)) {
-    return action;
-  } else {
-    return state;
+  switch (action.type) {
+    case "ADD":
+      return action.payload;
+    case "ERROR":
+      return state;
+    default:
+      return state;
+  }
+};
+
+const recipeDetailReducer = (state, action) => {
+  switch (action.type) {
+    case "LOADING":
+      return {
+        ...state,
+        loading: true
+      };
+    case "CHANGE":
+      return {
+        ...state,
+        current: action.id,
+        loading: false
+      };
+    case "ADD":
+      return {
+        ...state,
+        [action.id]: action.payload,
+        current: action.id,
+        loading: false
+      };
+    case "ERROR":
+      return {
+        ...state,
+        error: action.error,
+        loading: false
+      };
+    default:
+      state;
   }
 };
 
 export default () => {
   const [recipes, dispatch] = useReducer(recipesReducer, []);
+  const [recipeDetail, dispatchDetail] = useReducer(recipeDetailReducer, {
+    loading: false,
+    error: ""
+  });
 
   useEffect(
     () => {
       fetch(`http://reactrecipes.herokuapp.com/v1/recipes`)
         .then(res => res.json())
-        .then(json => dispatch(json));
+        .then(payload => dispatch({ type: "ADD", payload }))
+        .catch(error => dispatch({ type: "ERROR", error }));
     },
     [recipes]
   );
+
+  const onRecipeClick = id => {
+    if (recipeDetail.current === id) return;
+    const recipeExists = recipeDetail[id];
+    if (!!recipeExists) {
+      dispatchDetail({ type: "CHANGE", id });
+    } else {
+      dispatchDetail({ type: "LOADING" });
+      fetch(`http://reactrecipes.herokuapp.com/v1/recipes/${id}`)
+        .then(res => res.json())
+        .then(payload => dispatchDetail({ type: "ADD", id, payload }))
+        .catch(error => dispatchDetail({ type: "ERROR", error }));
+    }
+  };
+
   return (
     <Fragment>
       <Header />
       <main style={{ display: "flex" }}>
-        <RecipeList style={{ flex: 3 }} recipes={recipes} />
-        <RecipeDetail style={{ flex: 5 }} />
+        <Suspense
+          maxDuration={800}
+          fallback={
+            <div style={{ flex: 5 }}>
+              <h2>Recipe list is loading...</h2>
+            </div>
+          }
+        >
+          <RecipeList
+            style={{ flex: 3 }}
+            recipes={recipes}
+            handleClick={onRecipeClick}
+          />
+        </Suspense>
+        <RecipeDetail
+          style={{ flex: 5 }}
+          detail={recipeDetail[recipeDetail.current]}
+          error={recipeDetail.error}
+          loading={recipeDetail.loading}
+        />
       </main>
     </Fragment>
   );
--- src/components/Recipe/RecipeList.js
+++ src/components/Recipe/RecipeList.js
@@ -1,12 +1,12 @@
 import React, { memo } from "react";
 
-export default memo(({ style, recipes }) => {
+export default memo(({ handleClick, style, recipes }) => {
   return (
     <div style={style}>
       <h2>Recipes</h2>
       <ul>
         {recipes.map(({ id, name, category }) => (
-          <li key={id}>
+          <li key={id} onClick={() => handleClick(id)}>
             <span>{name}</span>
             <span>{category}</span>
           </li>
