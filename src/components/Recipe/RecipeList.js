a57efb8966276055297fa98cf1151e8ad386c5c8
StudentOfJS <rodlewis45@gmail.com>
2018-11-06 12:20:21 +0800

    RecipeList component test

--- src/components/Recipe/RecipeList.js
+++ src/components/Recipe/RecipeList.js
@@ -6,6 +6,7 @@ const RecipeList = ({ handleClick, isFavorite, onFavorite, recipes }) => (
   <ul className="list-reset" role="Menu">
     {recipes.map(({ id, name, category }) => (
       <RecipeListItem
+        key={id}
         onClickOrPress={handleClick}
         favorited={isFavorite(id)}
         id={id}
@@ -18,10 +19,17 @@ const RecipeList = ({ handleClick, isFavorite, onFavorite, recipes }) => (
 );
 
 RecipeList.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  isFavorite: PropTypes.func.isRequired,
-  onFavorite: PropTypes.func.isRequired,
-  recipes: PropTypes.arrayOf(PropTypes.object).isRequired,
+  handleClick: PropTypes.func,
+  isFavorite: PropTypes.func,
+  onFavorite: PropTypes.func,
+  recipes: PropTypes.arrayOf(PropTypes.object),
+};
+
+RecipeList.defaultProps = {
+  handleClick: () => {},
+  isFavorite: () => false,
+  onFavorite: () => {},
+  recipes: [],
 };
 
 export default RecipeList;
--- src/components/specs/RecipeListSpec.js
+++ src/components/specs/RecipeListSpec.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { BrowserRouter } from 'react-router-dom';
-import Header from '../Header';
+import RecipeList from '../Recipe/RecipeList';
 
-describe('<Header />', () => {
-  test('should render correctly', () => {
+const testRecipes = [
+  {
+    id: 1,
+    name: 'test recipe 1',
+    category: 'test category',
+  },
+  {
+    id: 2,
+    name: 'test recipe 2',
+    category: 'test category',
+  },
+  {
+    id: 3,
+    name: 'test recipe 3',
+    category: 'test category',
+  },
+  {
+    id: 4,
+    name: 'test recipe 4',
+    category: 'test category',
+  },
+];
+
+describe('<RecipeList />', () => {
+  test('should not break when no props passed', () => {
+    const component = renderer.create(<RecipeList />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('should render recipes correctly', () => {
+    const component = renderer.create(<RecipeList recipes={testRecipes} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('should render favorites correctly', () => {
     const component = renderer.create(
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>,
+      <RecipeList recipes={testRecipes} isFavorite={id => id > 0 && id < 2} />,
     );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
